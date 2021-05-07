00451b740796d8354e50623d6e420640334fd52f
Jackbaklava <>
2021-05-07 20:26:37 +0000

    Still trying to fix port error

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ const {client, loadClient} = require("./src/client.js");
 
 const express = require("express");
 const server = express();
-const port = process.env.port || 3000;
 
 
 server.get("/", (req, res) => {
   res.send("Server is live!")
 });
 
-server.listen(port, () => {
-  console.log(`Server listening at port ${port}.`)
+server.listen(process.env.port || 3000, () => {
+  console.log("Server is live!")
 });
 
 
