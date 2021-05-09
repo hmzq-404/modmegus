81a1d6673a0578000527f563651e31373da81ce9
Jackbaklava <>
2021-05-09 18:18:30 +0000

    Worked on message moderation

--- events/message.js
+++ events/message.js
@@ -11,5 +11,10 @@ module.exports = {
   async execute(message) {
     //Exit if author is bot or message is DM
     if (message.author.bot || message.guild === undefined) return;
+    
+    if (message.member.hasPermission("ADMINISTRATOR") === false) {
+      moderation.moderateMessage(message);
+    }
+    
   }
 }
--- src/moderation.js
+++ src/moderation.js
@@ -1,21 +1,29 @@
 "use strict";
 
 const bannedContent = require("../banned_content.json");
+const utils = require("../src/utils.js")
 
 
+function containsBannedWords(string) {
+  for (const word of bannedContent.words) {
+    if (string.includes(word)) {
+      return true;
+    }
+  }
+}
 
-async function hasBannedWords(message) {
+
+async function moderateMessage(message) {
   //"My Name is Bob" => "mynameisbob"
   const cleanedMessage = message.replaceAll(" ", "").lower();
   
-  for (const word of bannedContent.words) {
+  if (containsBannedWords(message.content)) {
     
   }
-  
 }
 
 
 
 module.exports = {
-  hasBannedWords
+  moderateMessage
 }
